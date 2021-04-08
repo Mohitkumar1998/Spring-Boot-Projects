c95bde1a1379949c48a408aabcc403c759d554c9
Mohit Kumar <mohit08kumar98@gmail.com>
2021-04-08 17:58:49 +0530

    First Project Commit

---
+++ springBootPreRequisite/src/main/java/com/wipro/springBootPreRequisite/SpringBootPreRequisiteApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.springBootPreRequisite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPreRequisiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPreRequisiteApplication.class, args);
+	}
+
+}
---
+++ springBootPreRequisite/src/test/java/com/wipro/springBootPreRequisite/SpringBootPreRequisiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.springBootPreRequisite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPreRequisiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
